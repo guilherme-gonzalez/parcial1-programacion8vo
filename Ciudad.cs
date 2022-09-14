06cbcc38c25e77db028ffd912e854b2f41a90ff7
Guilherme Gonzalez <74881628+guilherme-gonzalez@users.noreply.github.com>
2022-09-14 00:01:06 -0400

    Add files via upload

---
+++ Ciudad.cs
@@ -0,0 +1,10 @@
+﻿namespace api.clientes.Models
+{
+    public class Ciudad
+    {
+        public int Id { get; set; }
+        public string Nombre_Ciudad { get; set; }
+        public string Estado { get; set; }
+
+    }
+}
---
+++ Clientes.cs
@@ -0,0 +1,17 @@
+﻿namespace api.clientes.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public int Idciudad { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Nro_Documento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+        public string Ciudad { get; set; }
+        public string Nacionalidad { get; set; }
+        public int Edad { get; set; }
+    }
+}
